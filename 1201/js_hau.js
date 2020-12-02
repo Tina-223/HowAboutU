89ababf88017bc22eb88bffc90b3d80291852dd6
Jimin Choi <60884877+zmin9@users.noreply.github.com>
2020-12-02 19:36:36 +0900

    Add files via upload
    search.html로 이동이 안되는 오류 해결, 각 html마다 javascript 태그 추가함

--- 1201/js_hau.js
+++ 1201/js_hau.js
@@ -1,13 +1,12 @@
 let KEY;
 
-
 function getKeyword(){
     const keyword=document.getElementById("key").value;
     if(keyword===""){
         alert("검색어를 입력하세요");
     } else {
         KEY=keyword;
-        location.href="./search.html";
+        location.href='./search.html';
     }
 }
 
