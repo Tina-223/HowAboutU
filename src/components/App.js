b9af8d6ff809d18b98686736af5b347622a409f6
Tina-223 <71012505+Tina-223@users.noreply.github.com>
2020-12-03 07:43:31 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+// on top of everything, manage authentication
 import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
@@ -5,11 +6,13 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null); // beginning of userObj -> must be on top
   useEffect(()=> {
     // listening for change
     authService.onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -18,8 +21,11 @@ function App() {
   }, []);
   return (
   <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..." }
-    <footer>엿같다</footer>
+    {init ? (<AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+    ) : (
+      "Initializing..." 
+    )}
+    <footer>Health Assistant</footer>
     </>
   );
 }
