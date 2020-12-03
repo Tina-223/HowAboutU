550a41f335cc7419052e9bd5dac634c1f7243ba7
Yeonhui-ili <71022583+Yeonhui-ili@users.noreply.github.com>
2020-12-03 12:13:06 +0900

    Update Home.js
    onFileChange 정의

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
-import { dbService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
 import Record from "components/Record";
 
 const Home = ({userObj}) => {
     const [record, setRecord] = useState("");
     const [records, setRecords] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
         // snapshot : any change in database -> alert
         dbService.collection("records").onSnapshot((snapshot) => {
@@ -18,26 +20,56 @@ const Home = ({userObj}) => {
     }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
-        await dbService.collection("records").add({
-            text: record,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-        });
-        // submit(save) 이후 빈칸 만들기
-        setRecord("");
+        let attachmentUrl = "";
+        if (attachment !== "") {
+          const attachmentRef = storageService
+            .ref()
+            .child(`${userObj.uid}/${uuidv4()}`);
+          const response = await attachmentRef.putString(attachment, "data_url");
+          attachmentUrl = await response.ref.getDownloadURL();
+    }
+    const recordObj = {
+      text: record,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
     };
+    await dbService.collection("records").add(recordObj);
+    setRecord("");
+    setAttachment("");
+  };
     const onChange = (event) => {
         const {
             target: {value},
         } = event;
         setRecord(value);
     };
-    console.log(records);
+     const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
     return (
     <div>
         <form onSubmit={onSubmit}>
             <input value={record} onChange={onChange} type="text" placeholder="Writing My Daily Log" maxLength={120} />
             <input type="submit" value="Record" />
+            {attachment && (
+              <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>Clear</button>
+              </div>
+            )}
         </form>
         <div>
             {records.map((record) => (
