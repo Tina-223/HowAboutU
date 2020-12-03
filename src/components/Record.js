ebd98ef169e39cc002f018bb11a5c8c54f433f56
Yeonhui-ili <71022583+Yeonhui-ili@users.noreply.github.com>
2020-12-03 11:53:54 +0900

    Update Record.js
    summit 기능 버튼 수정

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,13 +1,14 @@
-import { dbService } from "fbase";
+import { dbService, storageService} from "fbase";
 import React, { useState } from "react";
 
-const Record = ({recordObj, isOwner}) => {
+const Record = ({ nweetObj: recordObj, isOwner }) => {
     const [editing, setEditing] = useState(false);  // for true false
     const [newRecord, setNewRecord] = useState(recordObj.text); // update text in input
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this log?");
         if (ok) {
             await dbService.doc(`records/${recordObj.id}`).delete();    // orange records == collection name
+            await storageService.refFromURL(recordObj.attachmentUrl).delete();
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev);
@@ -27,22 +28,26 @@ const Record = ({recordObj, isOwner}) => {
     };
     return (
         <div>
-            {
-                editing ? (
+            {editing ? (
                 <>
-                    {isOwner && (
-                        <>
-                            <form onSubmit={onSubmit}>
-                                <input type="text" placeholder="Edit your log" value={newRecord} required onChange={onChange} />
-                                <input type="submit" value="Update daily log" />
-                            </form>
-                            <button onClick={toggleEditing}>Cancel</button>
-                        </>
-                    )}
-                </>
+                   <form onSubmit={onSubmit}>
+                   <input
+                       type="text"
+                       placeholder="Edit your record"
+                       value={newRecord}
+                       required
+                       onChange={onChange}
+                      />
+                        <input type="submit" value="Update Record" />
+                      </form>
+                      <button onClick={toggleEditing}>Cancel</button>
+                    </>
                 ) : (
                 <>
                     <h4>{recordObj.text}</h4>
+                    {recordObj.attachmentUrl && (
+                      <img src={recordObj.attachmentUrl} width="50px" height="50px" />
+                    )}
                     {isOwner && (
                     <>
                         <button onClick={onDeleteClick}>Delete Log</button>
